8521637e2dd3d4bbd4b9501685117a0448a115a8
Kishan Patel <Kishan.Patel@intrideo.com>
2019-02-15 10:53:41 -0500

    Update

--- AboutMe/index.php
+++
@@ -1,7 +0,0 @@
-<h2>Hi<br>
-Please View Following Files.</h2>
-<a href="Chapter4.docx">More details about my current project.</a>
-<br>
-<a href="Resume.pdf">Kishan Resume</a>
-
-<h2>I am a quick learner too!</h2>
